1b7892234fbe74fa98321998714d12c5307e932b
ash982 <ash.ss.jiang@gmail.com>
2025-01-15 10:11:45 -0800

    Update input-singlefile.php

--- CrossFiles/input-singlefile.php
+++ CrossFiles/input-singlefile.php
@@ -1,1 +1,24 @@
+<?php
+// Simulating tainted input from user
+$taintedInput = $_GET['user_input'];
 
+// Create instances and pass tainted input
+$obj1 = new Class1($taintedInput);
+
+class Class1 {
+    private $input;
+
+    public function __construct($input) {
+        $this->input = $input;
+    }
+
+    public function processInput() {
+        echo "Class1 processing: " . $this->input . "\n";
+        // Potentially unsafe operation
+        eval("echo 'Evaluated in Class1: " . $this->input . "';");
+    }
+}
+
+// Use the classes
+$obj1->processInput();
+?>
