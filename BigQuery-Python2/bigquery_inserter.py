70412e7c9a04c6df817756307d8817220d55e5da
ash982 <ash.ss.jiang@gmail.com>
2025-01-22 20:58:38 -0800

    Update bigquery_inserter.py

--- BigQuery-Python2/bigquery_inserter.py
+++ BigQuery-Python2/bigquery_inserter.py
@@ -4,7 +4,7 @@ def insert_to_bigquery(customer, address):
     # Initialize BigQuery client
     client = bigquery.Client()
 
-    # Prepare the query
+    # Prepare the insecure query
     query = f"""
     INSERT INTO `your_project.your_dataset.your_table` (customer, address)
     VALUES ('{customer}', '{address}')
@@ -18,5 +18,24 @@ def insert_to_bigquery(customer, address):
     
     print(f"Inserted data for customer {customer} into BigQuery")
 
-# Note: In a production environment, you'd want to use parameterized queries
-# to prevent SQL injection vulnerabilities.
+
+    
+    # Prepare the insecure query2
+    table = "users"
+    columns = ["customer", "address", ]
+    values = ["Alice", "aliceasdfasd"]
+
+    query2 = "INSERT INTO {} ({}) VALUES ('{}', '{}');".format(
+    table, ", ".join(columns), *values
+)
+    # Execute the query
+    query_job2 = client.query(query)
+    
+    # Wait for the query to complete
+    query_job2.result()
+
+
+    #Prepare the insecure query3
+    query_job3 = client.query("""INSERT `{}.{}` (customer,address) VALUES("{}","{}")
+     """.format(customer,address))
+    query_job3.result()
