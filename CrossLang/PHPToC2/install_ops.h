28b6d43d7509a4f8b628cd69c1c3ea637b68a613
ash982 <ash.ss.jiang@gmail.com>
2025-01-26 10:14:03 -0800

    test

--- CrossLang/PHPToC2/install_ops.h
+++ CrossLang/PHPToC2/install_ops.h
@@ -1,10 +1,18 @@
 #ifndef INSTALL_OPS_H
 #define INSTALL_OPS_H
-#define(handler, handler_imp) 
-  
+
+// Define a macro to map a handler name to its implementation function
+#define OP_HANDLER(handler, handler_imp) \
+    static const struct HandlerMapping { \
+        const char *name; \
+        int (*function)(const char *); \
+    } handler##_mapping = { handler, handler_imp };
+
+// Function declarations
 int install_handler_imp(const char *name);
 char* validate(const char *name);
 
-OP_HANDLER("install-handler", install_handlder_imp)
+// Mapping the handler to its implementation
+OP_HANDLER("install-handler", install_handler_imp)
 
 #endif // INSTALL_OPS_H
