09c3f61d4173531b727adfbd4f4d5b7486db3543
ash982 <ash.ss.jiang@gmail.com>
2025-01-26 08:38:15 -0800

    Update install_ops.h

--- CrossLang/PHPToC2/install_ops.h
+++ CrossLang/PHPToC2/install_ops.h
@@ -1,1 +1,9 @@
+#ifndef INSTALL_OPS_H
+#define INSTALL_OPS_H
+
+int install_handler_imp(const char *name);
+char* validate(const char *name);
+
 OP_HANDLER("install-handler", install_handlder_imp)
+
+#endif // INSTALL_OPS_H
