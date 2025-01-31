cdce57981eb8fa2259b180249f13b57544e3d394
ash982 <ash.ss.jiang@gmail.com>
2025-01-30 16:35:35 -0800

    Create main.c

---
+++ CrossLang/bash2c/C/main.c
@@ -0,0 +1,31 @@
+#include "file_handler.h"
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+    FileHandler handler;
+    init_file_handler(&handler, "./remove_xml_2020.sh");
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Child process
+        handler.execute_script(&handler);
+        // If we reach here, exec failed
+        perror("exec failed");
+        exit(1);
+    } else if (pid > 0) {
+        // Parent process
+        int status;
+        waitpid(pid, &status, 0);
+        printf("Script execution completed with status %d\n", status);
+    } else {
+        // Fork failed
+        perror("fork failed");
+        return 1;
+    }
+
+    destroy_file_handler(&handler);
+    return 0;
+}
