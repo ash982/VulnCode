72631cc1a76774dbae849fb31877b0030151c485
ash982 <ash.ss.jiang@gmail.com>
2025-02-02 23:05:51 -0800

    Update sys.c

--- CrossLang/PHPToC2/sys.c
+++ CrossLang/PHPToC2/sys.c
@@ -1,7 +1,6 @@
 #include "sys.h"
 #include <stdlib.h>
 
-// Function to execute a system command safely
 int sys_call(const char *cmd) {
     if (cmd == NULL) {
         return -1;  // Error: null command
