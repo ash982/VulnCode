31228f1a3b2dec9598a81500f514d8216037c267
ash982 <ash.ss.jiang@gmail.com>
2025-03-03 18:50:39 -0800

    Update main.py

--- CrossFiles/Python/main.py
+++ CrossFiles/Python/main.py
@@ -1,6 +1,7 @@
 import sys
 from class1 import Class1
 from class2 import Class2
+from class3 import Class3
 
 def main():
     if len(sys.argv) < 2:
@@ -11,9 +12,16 @@ def main():
 
     obj1 = Class1(tainted_input)
     obj2 = Class2(tainted_input)
+    obj3 = Class3(tainted_input)
 
     obj1.process()
     obj2.process()
+    obj3.process_dangersous1()
+    obj3.process_dangersous2()
+    obj3.process_dangersous3()
+
+    obj3.process_safe1()
+    obj3.process_safe2()
 
 if __name__ == "__main__":
     main()
