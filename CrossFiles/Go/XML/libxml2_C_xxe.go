b9b88edc9267030c595487cffd75182b683491ba
ash982 <ash.ss.jiang@gmail.com>
2025-05-10 19:23:40 -0700

    Add files via upload

---
+++ CrossFiles/Go/XML/libxml2_C_xxe.go
@@ -0,0 +1,57 @@
+package main
+
+// #cgo pkg-config: libxml-2.0
+// #include <libxml/parser.h>
+// #include <libxml/tree.h>
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	// Initialize libxml2
+	C.xmlInitParser()
+	defer C.xmlCleanupParser()
+
+	xmlStr := C.CString(`<!DOCTYPE doc [<!ENTITY xxe SYSTEM "file:///etc/passwd">]>
+	<root>
+		<data>&xxe;</data>
+	</root>`)
+	defer C.free(unsafe.Pointer(xmlStr))
+
+	// Secure: Parse the XML document without entity substitution by default
+	// doc := C.xmlReadMemory(xmlStr, C.int(len(C.GoString(xmlStr))), nil, nil, 0)
+
+	// Insecure: Parse untrusted XML with entity substitution enabled
+	doc := C.xmlReadMemory(
+		xmlStr,                         // XML string
+		C.int(len(C.GoString(xmlStr))), // Length of XML string
+		nil,                            // URL (optional)
+		nil,                            // Encoding (optional)
+		C.XML_PARSE_NOENT,              // Parse external entities
+	)
+	if doc == nil {
+		fmt.Println("Error parsing XML")
+		return
+	}
+	defer C.xmlFreeDoc(doc)
+
+	// Get the root element
+	root := C.xmlDocGetRootElement(doc)
+	if root == nil {
+		fmt.Println("Error: No root element")
+		return
+	}
+
+	// Find the first child node named "data"
+	for child := root.children; child != nil; child = child.next {
+		if C.GoString((*C.char)(unsafe.Pointer(child.name))) == "data" {
+			// Print the content of the "data" node
+			if child.children != nil && child.children.content != nil {
+				fmt.Printf("Value: %s\n", C.GoString((*C.char)(unsafe.Pointer(child.children.content))))
+			}
+			break
+		}
+	}
+}
