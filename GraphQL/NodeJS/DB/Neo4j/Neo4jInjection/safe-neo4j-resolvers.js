e7935c1dc25de39bb1d064284136baf7f465d456
ash982 <ash.ss.jiang@gmail.com>
2025-05-18 23:05:01 -0700

    Create safe-neo4j-resolvers.js

---
+++ GraphQL/NodeJS/DB/Neo4j/Neo4jInjection/safe-neo4j-resolvers.js
@@ -0,0 +1,21 @@
+const driver = require('../config/neo4j-config');
+
+const neo4jResolvers = {
+  Query: {
+    personByName: async (_, { name }) => {
+      const session = driver.session();
+      try {
+        // Safe - uses parameters
+        const result = await session.run(
+          'MATCH (p:Person) WHERE p.name = $name RETURN p',
+          { name } // Parameters are passed as an object
+        );
+        return result.records.map(record => record.get('p').properties);
+      } finally {
+        await session.close();
+      }
+    }
+  }
+};
+
+module.exports = neo4jResolvers;
